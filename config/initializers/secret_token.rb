132960d79d5c2075da79b3fa9b2b523efc979dfb
Andrew S. Brown <andrew@helloenvoy.com>
2014-01-08 14:25:09 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CiNotifierProblem::Application.config.secret_token = 'ec6b6c232187a99bfde2dfc492cbb1a53c48c3de1292d45994b01dac05e6fc952320b4287dd1eece9d3aad703345956e71496ea3f7527443101b0fa10637926e'
